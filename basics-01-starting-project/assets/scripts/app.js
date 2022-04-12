541e24fd2d56116e572240467ab7e0ee000269b1
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-04-12 21:31:36 +0530

    Retouch with the scripting language and will continue it till the perfection

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,10 @@
+let currentResult = 0;
+currentResult += 10;
+let Description = `${currentResult} is the answer made by adding 10 as data
+loremsbdjsadbasjdhsjdhsakjdasdfgsfgfjksdghfsjkdhfg
+kajslfhdksjfh
+`; //Template Literal
+let escapeSequence = 'printing \' with the text';
+// \ always forces the js to skip the word next to be skipped.
+// outputResult(currentResult, Description);
+outputResult(currentResult, Description);
