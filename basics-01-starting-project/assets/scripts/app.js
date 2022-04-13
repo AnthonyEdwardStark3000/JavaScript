0f471968f4af0933ef062c75085a1d8245a14767
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-04-13 18:19:26 +0530

    Code Refactoring took place

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,10 +1,50 @@
-let currentResult = 0;
-currentResult += 10;
-let Description = `${currentResult} is the answer made by adding 10 as data
-loremsbdjsadbasjdhsjdhsakjdasdfgsfgfjksdghfsjkdhfg
-kajslfhdksjfh
-`; //Template Literal
-let escapeSequence = 'printing \' with the text';
-// \ always forces the js to skip the word next to be skipped.
-// outputResult(currentResult, Description);
-outputResult(currentResult, Description);
+const defaultValue = 0 ;
+let currentValue = defaultValue;
+
+function getUserInputasNumber(){
+    return parseInt(userInput.value);
+};
+
+function createResultLog(operator, previousValue, CurrentValue){
+    const calculationDescription = `${previousValue} ${operator} ${CurrentValue}`;     
+    outputResult(currentValue ,calculationDescription);
+}
+
+function add(){
+    const userInput = getUserInputasNumber();
+    // const calculationDescription = `${currentValue} + ${userInput}`;
+    // console.log(calculationDescription);
+    const initialValue = currentValue;
+    currentValue = currentValue + userInput ;
+    createResultLog('+',initialValue, userInput);
+    // outputResult(currentValue ,calculationDescription);
+};
+
+function subtract(){
+    const userInput = getUserInputasNumber();
+    const initialValue = currentValue;
+    currentValue = currentValue - userInput;
+    createResultLog('-', initialValue, userInput);      
+};
+
+function divide(){
+    const userInput = getUserInputasNumber();
+    const initialValue = currentValue;
+    currentValue = currentValue / userInput;
+    createResultLog('/', initialValue, userInput);      
+    
+};
+
+function multiply (){
+    const userInput = getUserInputasNumber();
+    const initialValue = currentValue;
+    currentValue = currentValue * userInput;
+    createResultLog('*', initialValue, userInput);      
+    
+};
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
