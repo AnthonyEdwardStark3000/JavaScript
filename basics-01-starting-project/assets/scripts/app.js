612997406d151beeeb5f197691a2c52315e75346
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-04-14 00:22:09 +0530

    Dive deeper into JS for feeling comfortable with React js

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,10 @@
 const defaultValue = 0 ;
 let currentValue = defaultValue;
+let logEntries = [];
+//Array is a special type of object in Javascript
+// check = [1,24,5]
+// console.log(typeof(check)) will print object
+//type of undefind is undefined , null is object, NaN is number.
 
 function getUserInputasNumber(){
     return parseInt(userInput.value);
@@ -10,36 +15,70 @@ function createResultLog(operator, previousValue, CurrentValue){
     outputResult(currentValue ,calculationDescription);
 }
 
+function objectLog(operationPerformed, previousVal, currentVal, userInput){
+    //Objects
+    const logEntry = {
+        operation: operationPerformed,
+        initialVal: previousVal,
+        numberEntered: userInput,
+        finalVal: currentVal
+    };
+
+    // logEntries.push(userInput);//Arrays
+    logEntries.push(logEntry); 
+    console.log(logEntries);
+    console.log(logEntry.operation); //Accessing a particular value from an object.
+}
+
+
 function add(){
     const userInput = getUserInputasNumber();
     // const calculationDescription = `${currentValue} + ${userInput}`;
     // console.log(calculationDescription);
     const initialValue = currentValue;
-    currentValue = currentValue + userInput ;
+    // currentValue = currentValue + userInput ;
+    currentValue += userInput ;
     createResultLog('+',initialValue, userInput);
     // outputResult(currentValue ,calculationDescription);
+    
+    // //Objects
+    // const logEntry = {
+    //     operation: "Addition",
+    //     initialVal: initialValue,
+    //     numberEntered: userInput,
+    //     finalVal: currentValue
+    // };
+
+    // // logEntries.push(userInput);//Arrays
+    // logEntries.push(logEntry); 
+    // console.log(logEntries);
+    // console.log(logEntry.operation); //Accessing a particular value from an object.
+    objectLog('Addition', initialValue, currentValue, userInput);
 };
 
 function subtract(){
     const userInput = getUserInputasNumber();
     const initialValue = currentValue;
-    currentValue = currentValue - userInput;
+    currentValue -= userInput;
     createResultLog('-', initialValue, userInput);      
+    objectLog('Subtraction', initialValue, currentValue, userInput);
 };
 
 function divide(){
     const userInput = getUserInputasNumber();
     const initialValue = currentValue;
-    currentValue = currentValue / userInput;
-    createResultLog('/', initialValue, userInput);      
+    currentValue /= userInput;
+    createResultLog('/', initialValue, userInput); 
+    objectLog('Division', initialValue, currentValue, userInput);     
     
 };
 
 function multiply (){
     const userInput = getUserInputasNumber();
     const initialValue = currentValue;
-    currentValue = currentValue * userInput;
-    createResultLog('*', initialValue, userInput);      
+    currentValue *= userInput;
+    createResultLog('*', initialValue, userInput);   
+    objectLog('Multiplication', initialValue, currentValue, userInput);   
     
 };
 
