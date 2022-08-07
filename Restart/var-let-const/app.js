9d88411b6e98bb944ee53dd9536b6df269f4a119
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-08-07 18:54:03 +0530

    Some concepts of the day

---
+++ Restart/var-let-const/app.js
@@ -0,0 +1,23 @@
+var myname = 'Mr.Stark';
+let myHobbies;
+if(myname == 'Mr.Stark')
+{
+    myHobbies = ['Playing','Creating','Learning'];
+}
+
+function greet(){
+    var myname = 'Suresh';
+    var age =22;
+    console.log(myname, age);
+}
+console.log(myname, myHobbies);
+greet();
+
+console.log(favouriteFood); //undefined
+var favouriteFood = 'pizza';
+
+// console.log(favouriteCar); // error saying favouriteCar is not initialized
+// let favouriteCar = 'Rolls Royce';
+
+luck = false ;
+console.log(luck);
