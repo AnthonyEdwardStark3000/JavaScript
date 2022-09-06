97c9103d6f2340464251bf9454da304bcfc4d301
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-09-06 21:23:11 +0530

    OOP

---
+++ Restart/oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+        title:'Pillow',
+        imageUrl:'http://google.com',
+        price:12,
+        description: 'Pillow is a pillow'
+    },
+    {
+        title:'Carpet',
+        imageUrl:'http://google.com',
+        price:19,
+        description: 'Carpet is a Carpet'
+    },
+];
