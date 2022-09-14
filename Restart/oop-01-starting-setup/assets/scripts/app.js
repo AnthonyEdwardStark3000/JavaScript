5fd9095eaf9e6a4047a90068bb1f77f4b20d14d9
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-09-14 21:58:36 +0530

    Have forgotten a bit

--- Restart/oop-01-starting-setup/assets/scripts/app.js
+++ Restart/oop-01-starting-setup/assets/scripts/app.js
@@ -13,7 +13,23 @@ class Product {
 }
 
 class ShoppingCart {
-    
+        items = [];
+
+        addProduct(product){
+            this.items.push(product);
+            this.totalOutput.innerHTML = `<h2>Total :${1}₹</h2>`;
+        }
+
+        render(){
+            const cartEl = document.createElement('section');
+            cartEl.innerHTML = `
+            <h2>Total ${0} ₹</h2>
+            <button>Order Now !</button>
+            `;
+            cartEl.className = 'cart';
+            this.totalOutput = cartEl.querySelector('h2');
+            return cartEl;
+        }
 }
 
 class ProductItem {
@@ -22,8 +38,7 @@ class ProductItem {
     }
 
     addToCart(){
-        console.log('add to cart');
-        console.log(this.product);
+        App.addProductToCart(this.product);
     }
 
 
@@ -53,7 +68,6 @@ class ProductList {
         new Product('Carpet','http://google.com','Carpet is a Carpet',19),
     ] 
     render(){
-        const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for(const prod of this.products){
@@ -61,9 +75,35 @@ class ProductList {
         const prodEl = prodItem.render();
         prodList.append(prodEl);
         }
-        renderHook.append(prodList);
+        return prodList;
     }
 }
 
-const productList = new ProductList();
-productList.render();
+class Shop{
+    render(){
+        const renderHook = document.getElementById('app');
+        this.cart = new ShoppingCart();
+        const cartEl = this.cart.render();
+        const productList = new ProductList();
+        const prodListEl = productList.render();
+       
+        renderHook.append(cartEl);
+        renderHook.append(prodListEl);
+    }
+}
+
+class App{
+    static cart;
+    
+    static init(){
+        const shop = new Shop();
+        shop.render();
+        this.cart = shop.cart;
+    }
+
+    static addProductToCart(product){
+        this.cart.addProduct(product); 
+    }
+}
+
+App.init();
